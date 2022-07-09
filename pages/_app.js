bb8480d65b9f3cf36cbbbadf269d4593bd0526e3
Tom <wexler18@gmail.com>
2022-07-09 00:02:25 -0400

    meta viewport added

--- pages/_app.js
+++ pages/_app.js
@@ -10,9 +10,11 @@ import Layout from "../components/Layout";
 const MyApp = ({ Component, pageProps }) => {
 
 return <>
-{/* <Head>
+<Head>
+    <title>Jesus The Good Shepherd</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     
-</Head> */}
+</Head>
 <Layout>
     <Component {...pageProps} />
 </Layout>
